fbe9aef4903cae42cc24aae9aa3eae04e7b42dfa
Yash <yashpande2002@gmail.com>
2022-05-29 23:43:55 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+get()
+let mobile=null;
+function handleChange(e){
+  // mobile=e.target.value;
+  console.log(e);console.log(e.target.value);console.log(mobile);
+}
+function App() {
+  return (
+     
+                <div>
+                  {/* hello */}
+                <input type="number" id="mobile" placeholder="Mobile Number" onChange={handleChange}/> 
+                <input type="password" id="password" placeholder="password"  /> 
+                </div>
+                   
+  );
+}
+
+export default App;
