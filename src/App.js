ba61050e2e97340169b096a541c5508e5070bb7c
kushteppalwar <kushteppalwar@gmail.com>
2022-06-05 01:46:14 +0530

    Event Details card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 
 import './App.css';
 
-get()
-let mobile=null;
-function handleChange(e){
-  // mobile=e.target.value;
-  console.log(e);console.log(e.target.value);console.log(mobile);
-}
-function App() {
-  return (
+import EventDetails from './Components/EventDetails';
+// get()
+// let mobile=null;
+// function handleChange(e){
+//   // mobile=e.target.value;
+//   console.log(e);console.log(e.target.value);console.log(mobile);
+// }
+// function App() {
+//   return (
      
-                <div>
-                  {/* hello */}
-                <input type="number" id="mobile" placeholder="Mobile Number" onChange={handleChange}/> 
-                <input type="password" id="password" placeholder="password"  /> 
-                </div>
+//                 <div>
+//                   {/* hello */}
+//                 <input type="number" id="mobile" placeholder="Mobile Number" onChange={handleChange}/> 
+//                 <input type="password" id="password" placeholder="password"  /> 
+//                 </div>
                    
-  );
-}
+//   );
+// }
 
-export default App;
+export default EventDetails;
