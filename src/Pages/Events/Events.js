7ac1f361b97c844913a5600ad25094309a5887c5
Yash <yashpande2002@gmail.com>
2022-06-05 05:30:34 +0530

    sample logo with animation added

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -6,51 +6,52 @@ const Events = () => {
     {
       EventName: "Event1",
      img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    
     },
     {
       EventName: "Event2",
-            img: "https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-" ,
+      img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-" ,logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",
     },
     {
       EventName: "Event3",
       
       img:"https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event4 ",
    
       img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event5",
     
       img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event6",
     
       img: "https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event7",
     
       img:"https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event8 ",
     
       img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event9",
    
       img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event10",
    
     img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
-    },
+logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     
     
   ];
@@ -64,19 +65,26 @@ const Events = () => {
       >
        Events
       </h1>
-      <div className="Events-main flex justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto">
+      <div className="Events-main flex justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto ">
         {teamDetails.map((member, i) => (
           <div
             key={`member${i}`}
             className="w-[300px] transition-all p-3 hover:bg-purple-600 hover:bg-opacity-40 inline-block m-4 cursor-pointer rounded-xl "
           >
             
-              <div className=" ">
-                <img
-                  className="max-h-full max-w-full  "
+              <div className="relative  hover:animate-none ">
+                
+              <img
+                  className="max-h-full max-w-full absolute px-12 hover:animate-bounce z-20 "
+                  src={member.logo}
+                  alt="Logo1"
+                />
+              <img
+                  className="max-h-full max-w-full z-10 pt-16 "
                   src={member.img}
                   alt="Events"
                 />
+                
               </div>
               <div>
                 <br />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+   'bounce': 'bounce 2.4s linear infinite'},
+},
   },
   plugins: [],
 }
