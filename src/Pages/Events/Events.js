86372b70deed94ee44927a3f04c913fa80fe3d28
Yash <yashpande2002@gmail.com>
2022-06-10 02:14:22 +0530

    grid designed with heading tech, non-tech

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -18,22 +18,28 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
     {
       EventName: "Event3",
       
-      img:"https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
+      img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event4 ",
    
-      img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
+      img: "https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event5",
     
       img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
-    {
-      EventName: "Event6",
     
-      img: "https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
+      
+    
+    
+  ];
+const Details=[
+  {
+  EventName: "Event6",
+    
+      img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventName: "Event7",
@@ -48,17 +54,14 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
     {
       EventName: "Event9",
    
-      img:"https://drive.google.com/uc?export=view&id=1sWjtBMDAZsLxkOAVZ35o8fGLK0CYaVj-",
+      img:"https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
     {
       EventNamT: "Event10",
    
     img: "https://drive.google.com/uc?export=view&id=1kqyU7WUULqPKL8_1TvWt3IuKrWV9akcA",
 logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-ReQ",    },
-    
-    
-  ];
-
+]
   return (
     
       <body className="bg-black">
@@ -73,8 +76,56 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
       </h1>
       
       
-      <div className=" flex justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto ">
+      <div className="  justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto grid grid-cols-2 gap-2">
+        <div className="text-center">
+          <h1 className="text-white text-center text-2xl text-bold">Technical</h1>
         {teamDetails.map((member, i) => (
+          <div
+            key={`member${i}`}
+            className="cardpink w-[310px] h-[320px] transition-all p-3  inline-block m-4 cursor-pointer rounded-xl   "
+          >
+          <a className=""> 
+                        <span className=" ">
+                          <span className="icon">
+                          <i class="far fa-shopping-cart text-white text-2xl px-64"></i>
+                          </span>
+                          {/* <span className=" px-20 text-white ">Add to cart</span> */}
+                        </span>
+                        
+                      </a>
+              <div className="relative   ">
+            <div className="">
+              <img
+                  className="Logo max-h-full max-w-full absolute px-12  z-20   "
+                  src={member.logo}
+                  alt="Logo1"
+                />
+       
+              <img
+                  className="base max-h-full max-w-full z-10 pt-16   "
+                  src={member.img}
+                  alt="Events"
+                />
+                         
+              </div>
+             
+              </div>
+                <br />
+                
+                <p className="text-white  text-center  p-text-white text-allign:center   px-8 rounded-2xl font-bold  cursor-pointer   ">
+                  {member.EventName}
+                </p>
+                
+            
+              </div>
+            
+            
+          
+         
+          
+        ))}</div>
+        <div className=""><h1 className="text-white text-center text-2xl text-bold">Non-Technical</h1>
+        {Details.map((member, i) => (
           <div
             key={`member${i}`}
             className="card w-[310px] h-[320px] transition-all p-3  inline-block m-4 cursor-pointer rounded-xl   "
@@ -118,7 +169,7 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
           
          
           
-        ))}
+        ))} </div>
       </div>
       
       
