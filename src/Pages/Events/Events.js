7ad9de71486d1db4190cd1450fd118d1a3272862
Yash <yashpande2002@gmail.com>
2022-07-20 18:11:48 +0530

    changes

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -76,9 +76,10 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
       </h1>
       
       
-      <div className="  justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto grid grid-cols-2 gap-2">
-        <div className="text-center">
-          <h1 className="text-white text-center text-2xl text-bold">Technical</h1>
+      
+      <div className="  justify-evenly flex-wrap w-full  text-center sm:hidden  ">
+        <div className="text-center justify-evenly flex-wrap w-full">
+          <h1 className="text-white text-center text-2xl text-bold ">Technical</h1>
         {teamDetails.map((member, i) => (
           <div
             key={`member${i}`}
@@ -124,6 +125,7 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
          
           
         ))}</div>
+        <div className="text-center justify-evenly flex-wrap w-full ">
         <div className=""><h1 className="text-white text-center text-2xl text-bold">Non-Technical</h1>
         {Details.map((member, i) => (
           <div
@@ -169,9 +171,104 @@ logo:"https://drive.google.com/uc?export=view&id=1f_Hr2NYr2XrecCxmIK23fNCLjsJo-R
           
          
           
-        ))} </div>
+        ))} </div> </div>
       </div>
-      
+      <div className="  justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-autogrid grid grid-cols-2 gap-2     ">
+        <div className="text-center justify-evenly flex-wrap w-full">
+          <h1 className="text-white text-center text-2xl text-bold ">Technical</h1>
+        {teamDetails.map((member, i) => (
+          <div
+            key={`member${i}`}
+            className="cardpink w-[310px] h-[320px] transition-all p-3  inline-block m-4 cursor-pointer rounded-xl   "
+          >
+          <a className=""> 
+                        <span className=" ">
+                          <span className="icon">
+                          <i class="far fa-shopping-cart text-white text-2xl px-64"></i>
+                          </span>
+                          {/* <span className=" px-20 text-white ">Add to cart</span> */}
+                        </span>
+                        
+                      </a>
+              <div className="relative   ">
+            <div className="">
+              <img
+                  className="Logo max-h-full max-w-full absolute px-12  z-20   "
+                  src={member.logo}
+                  alt="Logo1"
+                />
+       
+              <img
+                  className="base max-h-full max-w-full z-10 pt-16   "
+                  src={member.img}
+                  alt="Events"
+                />
+                         
+              </div>
+             
+              </div>
+                <br />
+                
+                <p className="text-white  text-center  p-text-white text-allign:center   px-8 rounded-2xl font-bold  cursor-pointer   ">
+                  {member.EventName}
+                </p>
+                
+            
+              </div>
+            
+            
+          
+         
+          
+        ))}</div>
+        <div className="text-center justify-evenly flex-wrap w-full ">
+        <div className=""><h1 className="text-white text-center text-2xl text-bold">Non-Technical</h1>
+        {Details.map((member, i) => (
+          <div
+            key={`member${i}`}
+            className="card w-[310px] h-[320px] transition-all p-3  inline-block m-4 cursor-pointer rounded-xl   "
+          >
+          <a className="">
+                        <span className=" ">
+                          <span className="icon">
+                          <i class="far fa-shopping-cart text-white text-2xl px-64"></i>
+                          </span>
+                          {/* <span className=" px-20 text-white ">Add to cart</span> */}
+                        </span>
+                        
+                      </a>
+              <div className="relative   ">
+            <div className="">
+              <img
+                  className="Logo max-h-full max-w-full absolute px-12  z-20   "
+                  src={member.logo}
+                  alt="Logo1"
+                />
+       
+              <img
+                  className="base max-h-full max-w-full z-10 pt-16   "
+                  src={member.img}
+                  alt="Events"
+                />
+                         
+              </div>
+             
+              </div>
+                <br />
+                
+                <p className="text-white  text-center  p-text-white text-allign:center   px-8 rounded-2xl font-bold  cursor-pointer   ">
+                  {member.EventName}
+                </p>
+                
+            
+              </div>
+            
+            
+          
+         
+          
+        ))} </div> </div>
+      </div>
       
       
       
