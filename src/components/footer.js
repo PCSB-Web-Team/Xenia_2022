9d88f00ff2cebfe1e455097759985a6f0347b992
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2022-07-28 04:35:45 +0530

    Modified footer files

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-// import xenialogo from src\Components\CSI Logo blue.png
+import xenialogo from '../assets/images/CSI_Logo_blue.png';
+
 export class Footer extends Component {
     render() {
         return (
