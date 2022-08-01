a77d029b89557fc5525ca9bcba75a24e1ca7a457
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-01 22:39:48 +0530

    signup formik added

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -20,79 +20,135 @@ const Login = () => {
           <input type="checkbox" id="chk-lg" aria-hidden="true" />
 
           {mode == "login" ? (
-            <>
-              <Formik
-                initialValues={{
-                  email: "",
-                  password: "",
-                }}
-              >
-                {(formik) => {
-                  return (
-                    <>
-                      <div class="signup text-center">
-                        <label
-                          className="label-lg"
-                          for="chk-lg"
-                          aria-hidden="true"
-                        >
-                          Sign up
-                        </label>
-                        <input
-                          className="input-lg"
-                          type="text"
-                          name="txt"
-                          placeholder="Full Name"
-                          required=""
-                        />
-                        <input
-                          className="input-lg"
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                          required=""
-                        />
-                        <input
-                          className="input-lg"
-                          type="password"
-                          name="pswd"
-                          placeholder="Password"
-                          required=""
-                        />
-                        <input
-                          className="input-lg"
-                          type="password"
-                          name="cnf pswd"
-                          placeholder="Confirm Password"
-                          required=""
-                        />
-                        <input
-                          className="input-lg"
-                          type="text"
-                          name="wno."
-                          placeholder="Whatsapp Number"
-                          required=""
-                        />
-                        <input
-                          className="input-lg"
-                          type="text"
-                          name="clg"
-                          placeholder="College"
-                          required=""
-                        />
-                        <button className="btn69">Submit</button>
-                        <button
-                          onClick={toggleMode}
-                          className="mx-auto p-2 bg-sky-600 px-4 m-auto rounded-md text-white font-bold"
-                        >
-                          Login
-                        </button>
-                      </div>
-                    </>
-                  );
-                }}
-              </Formik>
-            </>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                mobile: "",
+                college: "",
+              }}
+              validationSchema={Yup.object({
+                email: Validators.emailRequired,
+                password: Yup.string()
+                  .required("No password provided.")
+                  .min(8, "Password is too short - should be 8 chars minimum.")
+                  .matches(
+                    /[a-zA-Z]/,
+                    "Password can only contain Latin letters."
+                  ),
+                confirmPassword: Yup.string()
+                  .oneOf([Yup.ref("password"), null], "Passwords must match")
+                  .required("Enter Password"),
+                name: Validators.nameRequired,
+                mobile: Validators.mobileRequired,
+                college: Validators.name,
+              })}
+            >
+              {(formik) => {
+                return (
+                  <div class="px-4 space-y-4">
+                    <label className="label-lg" for="chk-lg" aria-hidden="true">
+                      Sign up
+                    </label>
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Name"}
+                        name={"name"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.name && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.name}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Email"}
+                        name={"email"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.email && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.email}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Password"}
+                        name={"password"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.password && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.password}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Confirm Password"}
+                        name={"confirmPassword"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.confirmPassword && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.confirmPassword}
+                        </div>
+                      )}
+                    </div>
+
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Whatsapp Number"}
+                        name={"mobile"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.mobile && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.mobile}
+                        </div>
+                      )}
+                    </div>
+
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"College"}
+                        name={"college"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.college && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.college}
+                        </div>
+                      )}
+                    </div>
+                    <button className="btn69">Submit</button>
+                    <button
+                      onClick={toggleMode}
+                      className="mx-auto p-2 bg-sky-600 px-4 m-auto rounded-md text-white font-bold"
+                    >
+                      Login
+                    </button>
+                  </div>
+                );
+              }}
+            </Formik>
           ) : (
             <Formik
               initialValues={{
@@ -120,30 +176,34 @@ const Login = () => {
                 return (
                   <div class="px-4 space-y-4">
                     <label className="label-lg">Login</label>
-                    <Field
-                      className="w-full rounded border bg-gray-100 border-gray-200 p-2"
-                      placeholder={"Email"}
-                      name={"email"}
-                      type={"text"}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.email && (
-                      <div className="text-red-500 font-bold">
-                        {formik.errors.email}
-                      </div>
-                    )}
-                    <Field
-                      className="w-full rounded border bg-gray-100 border-gray-200 p-2"
-                      placeholder={"Password"}
-                      name={"password"}
-                      type={"text"}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.password && (
-                      <div className="text-red-500 font-bold">
-                        {formik.errors.password}
-                      </div>
-                    )}
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Email"}
+                        name={"email"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.email && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.email}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <Field
+                        className="w-full rounded border bg-gray-100 border-gray-200 p-2"
+                        placeholder={"Password"}
+                        name={"password"}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.password && (
+                        <div className="text-red-500 font-light text-sm">
+                          {formik.errors.password}
+                        </div>
+                      )}
+                    </div>
                     <button
                       className="btn69 mb-4"
                       onClick={formik.handleSubmit}
