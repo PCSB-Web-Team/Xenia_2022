6a1665dc1bfd11dab38b57d44409517cc27e0f12
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-04 23:27:07 +0530

    Profile: merged with master

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ import Profile from "./pages/profile/profile";
 import { useSelector } from "react-redux";
 
 function App() {
-
-  const userState = useSelector(({ user }) => user)
+  const userState = useSelector(({ user }) => user);
   useEffect(() => {
     // const preloader = document.getElementById("preloader")
     // async function fetchToken() {
@@ -39,7 +38,7 @@ function App() {
     // setTimeout(() => {
     // preloader.style.display = "none";
     // }, 3000)
-  }, [userState])
+  }, [userState]);
 
   return (
     <div>
@@ -53,7 +52,6 @@ function App() {
           <Route path="/schedule" element={<Schedule />} loader={<Loader />} />
           <Route path="/events" element={<Events />} loader={<Loader />} />
           {/* <Route path="/side-events" element={<SideEvents />} loader={<Loader />} /> */}
-<<<<<<< HEAD
           <Route
             path="/event-details"
             element={<EventDetails />}
@@ -61,19 +59,6 @@ function App() {
           />
           <Route path="/profile" element={<Profile loader={<Loader />} />} />
           {/* <Route path="/cart" element={<Cart loader={<Loader />} />} /> */}
-=======
-          <Route path="/event-details" element={<EventDetails />} loader={<Loader />} />
-          {/* <Route path="/profile" element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          } loader={<Loader />} /> */}
-          {/* <Route path="/cart" element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          } loader={<Loader />} /> */}
->>>>>>> bf9041a802b62d9a4b9c6376e2f6b6adadf26b2a
           <Route path="/sponsors" element={<Sponsors loader={<Loader />} />} />
           {/* <Route path="/contact-us" element={<ContactUs loader={<Loader />} />} /> */}
           {/* <Route path="/web-team" element={<WebTeam loader={<Loader />} />} /> */}
