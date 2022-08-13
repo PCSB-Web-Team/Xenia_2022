39d3c8da1ffe2f018bd6afe8bd7705b6af87e063
Advait <naikadvait1610@gmail.com>
2022-08-13 13:07:16 +0530

    side events modified

--- src/pages/sideEvents/modals/modals.js
+++ src/pages/sideEvents/modals/modals.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./modals.css";
 
 export default function Modal(props) {
@@ -8,7 +8,7 @@ export default function Modal(props) {
     setModal(!modal);
   };
 
-  if (modal) {
+  if(modal) {
     document.body.classList.add('active-modal')
   } else {
     document.body.classList.remove('active-modal')
@@ -21,37 +21,41 @@ export default function Modal(props) {
       </button>
 
       {modal && (
-        <div className="modal">
+        <div className="modal ">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content  my-6 ">
             <h1 className="modalshd-sde mt-8 text-4xl ">Rules</h1>
             <hr />
-            <ul className="modalsp-sde ">
-              <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-            </ul>
-            <h1 className="modalshd-sde mt-8 text-4xl">Prizes</h1>
+              <ul className="modalsp-sde ">
+                <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+              </ul>
+              <h1 className="modalshd-sde mt-8 text-4xl">Prizes</h1>
             <hr />
-            <ul className="modalsp-sde ">
-              <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-            </ul>
-            <h1 className="modalshd-sde mt-8 text-4xl">Benifits</h1>
+              <ul className="modalsp-sde ">
+                <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+              </ul>
+              <h1 className="modalshd-sde mt-8 text-4xl">Benifits</h1>
             <hr />
-            <ul className="modalsp-sde ">
-              <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-            </ul>
-            <h1 className="modalshd-sde mt-8 text-4xl">Contact Us</h1>
+              <ul className="modalsp-sde ">
+                <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+              </ul>
+              <h1 className="modalshd-sde mt-8 text-4xl">Contact Us</h1>
             <hr />
-            <ul className="modalsp-sde ">
-              <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-              <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
-            </ul>
-
+              <ul className="modalsp-sde ">
+                <li className="pt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+                <li className="pt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimuslique nostrum nulla libero natus.</li>
+              </ul>
+              <div className="Modals-btn flex justify-center mr-10">
+              <button className="mt-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                Register
+              </button>
+              </div>
             <button className="close-modal " onClick={toggleModal}>
               X
             </button>
