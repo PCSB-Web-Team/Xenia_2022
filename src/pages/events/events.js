0cc4093a85d823e74a039939c2dc4ebd3430b0a3
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-15 01:26:35 +0530

    Event details modified

--- src/pages/events/events.js
+++ src/pages/events/events.js
@@ -9,130 +9,96 @@ import Request from "../../api/requests";
 
 const Events = () => {
   const [status, setStatus] = useState(1);
-  const [techEvents, setTechEvents] = useState([]);
+  const [techEvents, setTechEvents] = useState([
+    {
+      title: "Event",
+      description:
+        "description tech = res.data.data.filter((event) => event.isTechnical); nonTech = res.data.data.filter((event) => !event.isTechnical);",
+    },
+  ]);
   const [nonTechEvents, setNonTechEvents] = useState([]);
+  const [events, setEvents] = useState([]);
 
   async function fetchEventsData() {
-    await Request.getEvents().then((res) => {
-      let tech = [];
-      let nonTech = [];
-
-      tech = res.data.data.filter((event) => event.isTechnical);
-      nonTech = res.data.data.filter((event) => !event.isTechnical);
-
-      setTechEvents(tech);
-      setNonTechEvents(nonTech);
-    }).catch((err) => {
-      console.error("Error in get req:", err);
-    })
+    await Request.getEvents()
+      .then((res) => {
+        let tech = [];
+        let nonTech = [];
+        tech = res.data.data.filter((event) => event.isTechnical);
+        nonTech = res.data.data.filter((event) => !event.isTechnical);
+        setTechEvents(tech);
+        setNonTechEvents(nonTech);
+        setEvents(tech);
+      })
+      .catch((err) => {
+        console.error("Error in get req:", err);
+      });
   }
 
   useEffect(() => {
     fetchEventsData();
-  }, [])
+  }, []);
 
   return (
-    <div className="Events">
+    <div className="Events bg-gradient-to-bl from-black to-slate-800">
       <h1
         className="title text-6xl font-semibold text-center text-white pb-12 "
         style={{ fontFamily: "Quicksand" }}
       >
         Events
       </h1>
-      <div className="tabs">
+      <div className="flex space-x-8 justify-center  text-2xl font-thin tracking-widest text-gray-400 mb-2">
         <button
           type="button1"
           onClick={() => {
+            setEvents(techEvents);
             setStatus(1);
           }}
-          className={status === 1 ? "tabs-btn active-tab1" : "tabs-btn"}
+          className={` px-4 ${status === 1 && "border-b border-cyan-600"}`}
         >
           Technical
         </button>
         <button
           type="button2"
           onClick={() => {
+            setEvents(nonTechEvents);
             setStatus(2);
           }}
-          className={status === 2 ? "tabs-btn active-tab2" : "tabs-btn"}
+          className={` px-4 ${status === 2 && "border-b border-cyan-600"}`}
         >
-          Non- Technical
+          Non-Technical
         </button>
       </div>
       <div className="justify-evenly flex-wrap w-full xl:w-6/5 xl:mx-auto text-center">
-        {status === 1 ? (
-          <div className="text-center">
-            {/* <h1 className="text-purple-400 text-center text-2xl text-sebold">
-                Technical
-              </h1> */}
-            {techEvents.map((event) => (
+        <div className="text-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+          {events.map((event) => (
+            <Link
+              to={`/events/${event._id}`}
+              onClick={() => (document.documentElement.scrollTop = 0)}
+            >
               <div
                 key={event._id}
-                className="Eventcard w-[320px] h-auto transition-all inline-block m-4 cursor-pointer rounded-xl"
+                className="group w-[450px] grid grid-cols-3 items-center h-auto min-h-[200px] backdrop-blur-md transition-all m-4 cursor-pointer overflow-hidden hover:scale-110 ease-in-out"
               >
-                <Link
-                  to={`/events/${event._id}`}
-                  // onClick={() => console.log("heloo")}
-                  onClick={() => (document.documentElement.scrollTop = 0)}
-                >
-                  <div className="Eventcard_stars pb-16">
-                    <img src={stars1} alt="str1" className="str1 absolute" />
-                    <img src={stars2} alt="str2" className="str2 absolute" />
-                  </div>
-                  <div className="logo pt-12">
-                    <img src={event.logo} alt="logo" />
-                  </div>
-                  <div className="base">
-                    <img src={purple1} alt="base" />
-                  </div>
-                  <h3 className="event_heading text-white text-3xl pt-5">
+                <div className=" p-4 h-full col-span-1 flex items-center bg-black/10">
+                  <img src={event.logo} className="max-h-full max-w-full"></img>
+                </div>
+                <div className=" p-4 h-full col-span-2 text-left bg-gradient-to-br from-cyan-800/10 to-black/80 space-y-2 text-gray-400 group-hover:bg-cyan-700 ">
+                  <div className="border-b border-slate-600 text-cyan-300 font-bold text-lg pb-2 group-hover:text-black">
                     {event.name}
-                  </h3>
-                  <h4 className="event_discription text-white pt-1">
-                    {event.name}
-                  </h4>
-                </Link>
+                  </div>
+                  <div className="font-light text-sm h-[100px] overflow-auto group-hover:text-white">
+                    {event.details}
+                  </div>
+                  <div>{event.date}</div>
+                </div>
               </div>
-            ))}
-          </div>
-        ) : (
-          <div className="text-center">
-            {/* <h1 className="text-lime-400 text-center text-2xl text-bold">
-                Non-Technical
-              </h1> */}
-            {nonTechEvents.map((event) => (
-              <div
-                key={event._id}
-                className="Eventcard w-[320px] h-auto transition-all p-3 inline-block m-4 cursor-pointer rounded-xl   "
-              >
-                <Link
-                  to={`/events/${event._id}`}
-                  onClick={() => (document.documentElement.scrollTop = 0)}
-                >
-                  <div className="Eventcard_stars pb-20">
-                    <img src={stars1} alt="str1" className="str1 absolute" />
-                    <img src={stars2} alt="str2" className="str2 absolute" />
-                  </div>
-                  <div className="logo pt-12">
-                    <img src={event.logo} alt="logo" />
-                  </div>
-                  <div className="base">
-                    <img src={green1} alt="base" />
-                  </div>
-                  <h3 className="event_heading text-white text-3xl pt-5">
-                    {event.name}
-                  </h3>
-                  <h4 className="event_discription text-white pt-1">
-                    {event.name}
-                  </h4>
-                </Link>
-              </div>
-            ))}
-          </div>
-        )}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Events;
