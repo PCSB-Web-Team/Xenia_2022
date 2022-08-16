d023f405152dc2474f069426b04458027f910c9c
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-17 00:47:00 +0530

    sponsors page added

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,24 +1,46 @@
-import './sidebar.css';
+import "./sidebar.css";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CSILogo from "../assets/images/csi.png";
 
 const Sidebar = () => {
-    return (
-        <div className='social-links-panel'>
-            <div className='panel-container'>
-                <div><a href='https://www.pictcsi.com/' target='_blank' className='csi-logo-social'><img src={CSILogo} alt='' /></a></div>
-                <div><a href='http://www.facebook.com/csipict' target='_blank' className='far fb-logo text-white'>< i className='fa-brands fa-linkedin fa-lg' /></a></div>
-                <div><a href='http://www.instagram.com/csipict' target='_blank' className='ig-logo'><i className="fa-instagram-i" /></a></div>
-                <div><a href='http://www.linkedin.com/company/pict-csi' target='_blank' className='in-logo'><i className='fa-brands fa-linkedin fa-lg' /></a></div>
-                <div><a href='https://www.youtube.com/channel/UC_lcyqJpGxd7XWBMrcbpgpw/featured' target='_blank' className='yt-logo'><i className='fa fa-youtube fa-lg' /></a></div>
+  return (
+    <div className="fixed top-0 left-0 w-[50px] h-screen flex items-center justify-center">
+      <div className="w-full space-y-4 bg-black/20 backdrop-blur-xl h-auto p-2">
+        {socialMediaData.map((media) => {
+          return (
+            <div>
+              <a href={media.link} target="_blank" className="w-full">
+                {media.logo}
+              </a>
             </div>
-            <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' />
-            <link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.11.2/css/all.css' />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-
-    )
-}
+const socialMediaData = [
+  {
+    logo: <img src={CSILogo} alt="" />,
+    link: "https://www.pictcsi.com/",
+  },
+  {
+    logo: <i className="fa-brands fa-linkedin fa-lg" />,
+    link: "http://www.facebook.com/csipict",
+  },
+  {
+    logo: <img src={CSILogo} alt="" />,
+    link: "http://www.instagram.com/csipict",
+  },
+  {
+    logo: <img src={CSILogo} alt="" />,
+    link: "http://www.linkedin.com/company/pict-csi",
+  },
+  {
+    logo: "",
+    link: "https://www.youtube.com/channel/UC_lcyqJpGxd7XWBMrcbpgpw/featured",
+  },
+];
 
 export default Sidebar;
