4edeaf91806ee8f82ea4f10f11a63c5d3886fd40
Amit Purohit <amitpurohit4747@gmail.com>
2022-08-17 20:57:29 +0530

    404 page done

--- src/pages/404/Error.js
+++ src/pages/404/Error.js
@@ -1,9 +1,18 @@
+import React from "react";
+import bitcoin from "../../assets/images/bitcoin.png";
 import "./Error.css";
 
 const Error = () => {
   return (
     <div className="error-page">
-      
+      <h1>OOPS!</h1>
+      <h5>Something went wrong :(</h5>
+      <p>Error 404 Page not found</p>
+      <div>
+        <span>4</span>
+        <img src={bitcoin} alt="404" />
+        <span>4</span>
+      </div>
     </div>
   )
 }
