0b0509df6d8c7caee9fd7b3a3d1690ac412f7dd5
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-18 23:25:45 +0530

    events responsiveness solved

--- src/pages/events/events.js
+++ src/pages/events/events.js
@@ -36,7 +36,7 @@ const Events = () => {
   }, []);
 
   return (
-    <div className="Events bg-gradient-to-bl">
+    <div className="Events bg-gradient-to-bl bg-black/40">
       <div className="text-6xl font-semibold text-center text-white pb-12">
         Events
       </div>
@@ -66,22 +66,22 @@ const Events = () => {
             to={`/events/${event._id}`}
             onClick={() => (document.documentElement.scrollTop = 0)}
           >
-            <div className="group w-min h-min relative hover:scale-105 ease-in-out transition-all inline-block m-4">
+            <div className="group relative hover:scale-105 ease-in-out transition-all inline-block m-4">
               {/* <div className=" flex items-center absolute -top-2 z-20 rounded-full -right-4 px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-extralight">
                 15 Aug
               </div> */}
               <div
                 key={event._id}
-                className="event-card w-80 md:w-[400px] grid grid-cols-3 items-center h-auto min-h-[200px] backdrop-blur-md cursor-pointer overflow-hidden"
+                className="event-card w-full md:w-[400px] grid grid-cols-3 items-center h-auto min-h-[200px] backdrop-blur-xl cursor-pointer overflow-hidden"
               >
                 <div className=" p-4 col-span-1 flex items-center bg-black/60 h-[150px]">
                   <img src={event.logo} className="max-h-full max-w-full"></img>
                 </div>
-                <div className=" event-card-body p-4 h-full col-span-2 text-left bg-gradient-to-br from-cyan-600 to-black/20  space-y-2 text-gray-400 group-hover:bg-cyan-700 ">
-                  <div className="border-b border-slate-600 text-cyan-400 font-bold text-lg pb-2 group-hover:text-white">
+                <div className=" event-card-body p-4 h-full col-span-2 text-left bg-gradient-to-br from-black/10 to-black/50  space-y-2 text-gray-400 group-hover:bg-cyan-700 ">
+                  <div className="border-b border-slate-600 text-transparent font-bold text-lg pb-2 group-hover:text-white bg-clip-text bg-gradient-to-r bg-conic-to-bl bg-conic-to-l from-rose-500 to-indigo-700">
                     {event.name}
                   </div>
-                  <div className="font-light text-sm h-[100px] overflow-auto group-hover:text-white">
+                  <div className="font-light text-sm h-[100px] overflow-auto group-hover:text-white text-gray-300">
                     {event.details}
                   </div>
                   <div>{event.date}</div>
