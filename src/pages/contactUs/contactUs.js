56ac6e4c10c9a75ca39360bc1df3542799077c7e
VanshTeppalwar <vanshteppalwar@gmail.com>
2022-08-18 23:16:39 +0530

    Contact Page Updated

--- src/pages/contactUs/contactUs.js
+++ src/pages/contactUs/contactUs.js
@@ -1,145 +1,116 @@
-import "./contactUs.css";
-import PageBackground from "../../components/pageBackground";
-import { useSelector } from "react-redux/es/exports";
-import { Validators } from "../../utils";
-import { Field, Formik } from "formik";
-import * as Yup from "yup";
+import React, { useState, useRef } from 'react';
+import './contactUs.css';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+const ContactUs = () => {
+	const [name, setName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+	const reRef = useRef();
 
-function ContactUs() {
-  const {
-    userDetails: { name, mobile },
-  } = useSelector(({ user }) => user);
+	const Submit = async (e) => {
+		e.preventDefault();
 
-  return (
-    <div className="contact-us">
-      <PageBackground />
-      <div className="container">
-        <div className="screen">
-          {/* <div className="screen-header">
-                        <div className="screen-header-left">
-                            <div className="screen-header-button close"></div>
-                            <div className="screen-header-button maximize"></div>
-                            <div className="screen-header-button minimize"></div>
-                        </div>
-                        <div className="screen-header-right">
-                            <div className="screen-header-ellipsis"></div>
-                            <div className="screen-header-ellipsis"></div>
-                            <div className="screen-header-ellipsis"></div>
-                        </div>
-                    </div> */}
-          <div className="screen-body">
-            <div className="screen-body-item left md:rounded-tl-xl">
-              <div className="app-title">
-                <span>ABOUT</span>
-                <span>US</span>
-              </div>
-            </div>
-            <div className="screen-body-item md:rounded-tr-xl">
-              <p>
-                XENIA is an Annual Technical Fiesta organized by PICT CSI
-                Student Branch, formerly known as Enthusia. It is a three day
-                technical fest where bright minds all around participate to
-                compete amongst themselves. The organizing team in XENIA
-                consistently strives in exploring various technologies and
-                brings you the most innovative events. XENIA emphasizes on the
-                technical events while also bringing you the opportunity of
-                participating in various ingenious non-technical events.
-              </p>
-            </div>
-          </div>
-          <div className="screen-body">
-            <div className="screen-body-item left md:rounded-bl-xl">
-              <div className="app-title">
-                <span>CONTACT</span>
-                <span>US</span>
-              </div>
-            </div>
-            <div className="screen-body-item md:rounded-br-xl">
-              <Formik
-                initialValues={{
-                  name: name || "",
-                  mobile: mobile || "",
-                  email: "",
-                  message: "",
-                }}
-                validationSchema={Yup.object({
-                  email: Validators.emailRequired,
-                  mobile: Validators.mobileRequired,
-                  password: Validators.stringRequired,
-                })}
-                onSubmit={async (values) => {
-                  // Requests.contactUsForm(values)  //! We don't have the API endpoint for this request yet
-                  // .then((res) => {console.log(res);})
-                  // .catch((err) => {console.log(err);})
-                }}
-              >
-                {(formik) => {
-                  return (
-                    <div className="app-form">
-                      <div className="app-form-group">
-                        <Field
-                          className="app-form-control"
-                          type={"text"}
-                          name={"name"}
-                          placeholder={"NAME"}
-                          onChange={formik.handleChange}
-                          required
-                        />
-                      </div>
-                      <div className="app-form-group">
-                        <Field
-                          className="app-form-control"
-                          type={"email"}
-                          name={"email"}
-                          placeholder={"EMAIL"}
-                          onChange={formik.handleChange}
-                          required
-                        />
-                      </div>
-                      {formik.errors.email && (
-                        <div className="text-red-500 font-light text-sm">
-                          {formik.errors.email}
-                        </div>
-                      )}
-                      <div className="app-form-group">
-                        <Field
-                          className="app-form-control"
-                          type={"number"}
-                          name={"mobile"}
-                          placeholder={"CONTACT NO"}
-                          onChange={formik.handleChange}
-                          required
-                        />
-                        {formik.errors.mobile && (
-                          <div className="text-red-500 font-light text-sm">
-                            {formik.errors.mobile}
-                          </div>
-                        )}
-                      </div>
-                      <div className="app-form-group message">
-                        <Field
-                          className="app-form-control"
-                          type={"textarea"}
-                          name={"message"}
-                          placeholder={"MESSAGE"}
-                          onChange={formik.handleChange}
-                          required
-                        />
-                      </div>
-                      <div className="app-form-group buttons">
-                        <button className="app-form-button">CANCEL</button>
-                        <button className="app-form-button">SEND</button>
-                      </div>
-                    </div>
-                  );
-                }}
-              </Formik>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+		// const captchaToken = await reRef.current.executeAsync();
+		// reRef.current.reset();
+    console.log(e);
+		const data = { name, phone, email, message};
+    await axios.post('https://xenia-mailer.herokuapp.com/api/contact-us', data)
+    .then((response) => {
+      // console.log(response);
+      if (response.data.success) {
+        window.alert("Thank You For Contacting");
+        setName('');
+        setPhone('');
+        setEmail('');
+        setMessage('');
+        navigate(-1);
+		  }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+		
+	};
+
+	return (
+		<div className="contactUs mb-4 mt-20">
+			{/* <ReCaptcha
+				sitekey="6LdP8iobAAAAAHHXrhWEJLuoQWruhsQiLq1AywMg"
+				size="invisible"
+				ref={reRef}
+			/> */}
+			<div className="contactForm mb-4">
+				<h1 className='text-4xl font-mono font-bold '>Send us a message</h1>
+				<form onSubmit={Submit} id="contactUsForm">
+					<input
+						type="text"
+						placeholder="Your Name"
+						id="nameInputId"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						className="contactFill"
+						required={true}
+					></input>
+					<input
+						type="email"
+						placeholder="Email"
+						id="emailInputId"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						className="contactFill"
+						required={true}
+					></input>
+					<input
+						type="number"
+						min="7000000000"
+						max="9999999999"
+						placeholder="Phone"
+						id="phoneInputId"
+						value={phone}
+						onChange={(e) => setPhone(e.target.value)}
+						className="contactFill"
+					></input>
+					<textarea
+						placeholder="Message"
+						id="messageInputId"
+						className="contactFill"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+					></textarea>
+
+					<button type="submit" className="themeButtCont text-white border-sky-500 border-solid border-2" >
+						{/* <ThemeButton value="Submit" /> */}
+            Submit
+					</button>
+				</form>
+			</div>
+
+			<div className="contactInfo mb-2 mt-8">
+				<h1 className='text-2xl font-mono font-bold'>Contact info</h1>
+				<div className="address">
+					<h3 className='text-xl font-semibold'>Where to find us</h3>
+					<p>Survey no. 27, </p>
+					<p>Near Trimurti Chowk,</p>
+					{/* <p>Bharati Vidyapeeth Campus,</p> */}
+					<p>Dhankawadi, </p>
+					<p>Pune-411043</p>
+				</div>
+				<div className="phoneAndMail">
+					<p className='display-inline'>
+						<i className="fa fa-phone"></i>{' '}
+						<span >+91 7028929568 </span>
+					</p>
+					<p className='display-inline'>
+						<i className="fa fa-envelope"></i>{' '}
+						<span style={{ marginLeft: '5px' }}>support@pictcsi.com</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ContactUs;
