e97561ef35102998fe2627d18dbe70b130d53d0b
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-20 01:45:26 +0530

    push

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,7 +4,7 @@ import CSILogo from "../assets/images/csi.png";
 
 const Sidebar = () => {
   return (
-    <div className="fixed invisible md:visible top-0 left-0 w-[50px] h-screen flex items-center justify-center">
+    <div className="fixed invisible md:visible top-0 left-0 w-[50px] h-screen flex items-center justify-center z-10">
       <div className="w-full space-y-4 bg-black/20 backdrop-blur-xl h-auto p-2">
         {socialMediaData.map((media) => {
           return (
