6ecb92abaa2a584f54100c81316e1412ea745540
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-19 19:36:06 +0530

    webteam sorted

--- src/pages/webTeam/webTeam.js
+++ src/pages/webTeam/webTeam.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./WebTeam.css";
+import "./webTeam.css";
 import ProfileCard from "./TeamProfile";
 // import Vedant from "../../Assets/Images/WebTeam/vedant.jpeg";
 // import Gaurav from "../../Assets/Images/WebTeam/gaurav.jpg";
@@ -8,8 +8,6 @@ import ProfileCard from "./TeamProfile";
 // import Siddhesh from "../../Assets/Images/WebTeam/siddhesh.jpg";
 // import Pranil from "../../Assets/Images/WebTeam/pranil.jpeg";
 // import Kshitij from '../../Assets/Images/WebTeam/kshitij.jpg';
-
-
 
 let teamDetails = [
   {
@@ -58,47 +56,47 @@ let prernaSthhall = [
 ];
 
 let team2 = [
-    {
-      name: "Renuka Saraf",
-      linkedIn: "https://www.linkedin.com/in/renuka-saraf-538846240/",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657819990252.jpg",
-    },
-    {
-      name: "Dhiraj Thorat",
-      linkedIn: "https://www.linkedin.com/in/dhiraj-thorat-073970215",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658575296915.jpg",
-    },
-    {
-      name: "Atharva Kinikar",
-      linkedIn: "https://www.linkedin.com/in/atharva-kinikar/",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657699710579.jpg",
-    },
-  
-    {
-      name: "Raj Shende",
-      linkedIn: "https://www.linkedin.com/in/raj-shende-350718204",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657699545232.jpg",
-    },
-  ];
+  {
+    name: "Renuka Saraf",
+    linkedIn: "https://www.linkedin.com/in/renuka-saraf-538846240/",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657819990252.jpg",
+  },
+  {
+    name: "Dhiraj Thorat",
+    linkedIn: "https://www.linkedin.com/in/dhiraj-thorat-073970215",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658575296915.jpg",
+  },
+  {
+    name: "Atharva Kinikar",
+    linkedIn: "https://www.linkedin.com/in/atharva-kinikar/",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657699710579.jpg",
+  },
 
-  let team3 = [
-    {
-      name: "Vansh Teppalwar",
-      linkedIn: "https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658743938457.jpg",
-    },
-    {
-      name: "Vaishnavi Katkar",
-      linkedIn: "https://www.linkedin.com/in/vaishnavi-katkar-0a61a9211",
-      pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658766834425.jpg",
-    },
-    
-    {
-      name: "Devraj Shetake",
-      linkedIn: "https://www.linkedin.com/in/devraj-shetake-439606206/1",
-      pic: "https://nth22.s3.ap-south-1.amazonaws.com/devraj.jpg",
-    },
-  ];
+  {
+    name: "Raj Shende",
+    linkedIn: "https://www.linkedin.com/in/raj-shende-350718204",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1657699545232.jpg",
+  },
+];
+
+let team3 = [
+  {
+    name: "Vansh Teppalwar",
+    linkedIn: "https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658743938457.jpg",
+  },
+  {
+    name: "Vaishnavi Katkar",
+    linkedIn: "https://www.linkedin.com/in/vaishnavi-katkar-0a61a9211",
+    pic: "https://surveyheartmedia.s3.ap-south-1.amazonaws.com/files/eb7d93792dc6258a110491d24d9d5f/62ca636b7dffdc1069d3189f/sh_1658766834425.jpg",
+  },
+
+  {
+    name: "Devraj Shetake",
+    linkedIn: "https://www.linkedin.com/in/devraj-shetake-439606206/1",
+    pic: "https://nth22.s3.ap-south-1.amazonaws.com/devraj.jpg",
+  },
+];
 
 const webTeam = (props) => {
   let team = teamDetails.map((member) => {
@@ -135,15 +133,15 @@ const webTeam = (props) => {
   return (
     <div className="webTeam-wb" id="webTeam-wb">
       {/* <header className="page-headers-wb"> */}
-        <h1 className="header-name-wb text-4xl p-6">PCSB WEB TEAM</h1>
+      <h1 className="header-name-wb text-4xl p-6">PCSB WEB TEAM</h1>
       {/* </header> */}
       {team}
-      <br/>
+      <br />
       {teTeam}
       <br />
-       {seTeam}
-       <br />
-       {bTeam}
+      {seTeam}
+      <br />
+      {bTeam}
     </div>
   );
 };
