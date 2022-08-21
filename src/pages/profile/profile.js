533a2d0ae97b54d9331cbc135548fa98416d2ef2
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2022-08-22 04:42:42 +0530

    fixed profile page

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,30 +1,15 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Requests from "../../api/requests";
-import { setParticipations } from "../../store/middleware";
-
-// TODO: to be fetched from backend in useEffect
+import { AuthVerify } from "../../utils/authVerify";
 
 function RegisteredEventCard(eve) {
-  const [details, setDetails] = useState({
-    name: "CodeStrike",
-    teamSize: 2,
-    teamId: "123",
-    platform: "PCSB Coding Platform",
-    schedule: [
-      { round: 1, datetime: "11 June, 10:10 AM" },
-      { round: 2, datetime: "12 June, 10:10 AM" },
-    ],
-    link: "google.com",
-  });
+  const [details, setDetails] = useState({});
 
-  // TODO: fetch the event details using the id from backend
   useEffect(() => {
-    Requests.getEventById(eve.details.eventId).then((res) => {
+    Requests.getEventById(eve.details?.eventId).then((res) => {
       res = res.data;
       if (res.status) {
-        console.log(res.data);
         setDetails(res.data);
       } else {
         alert(res.error);
@@ -35,33 +20,38 @@ function RegisteredEventCard(eve) {
   return (
     <div className="p-4 bg-gray-200/20 backdrop-blur space-y-4 hover:bg-gray-200/10 transition">
       <div className="text-lg md:text-3xl text-blue-400 font-bold">
-        {details.name}
+        {details?.name}
       </div>
-      {details.teamSize > 1 && (
+      {details?.teamSize > 1 && (
         <>
           <div className="text-white">
-            Team ID: {eve.details.teamId || "N.A"}
+            Team ID: {eve.details?.teamId || "N.A"}
           </div>
         </>
       )}
       <div className="text-gray-300 font-thin">
-        Platform: {details.platform}
+        {details?.platform?.map(platform => (
+          <div>
+            <span>Round {platform?.round}</span>{": "}
+            <a href={platform?.link} target="_blank" rel="noopener noreferrer">{platform?.name}</a>
+          </div>
+        ))}
       </div>
       <div className="grid gap-2 text-sm">
         <div className="text-blue-500 font-bold">Date and Time</div>{" "}
-        {details.schedule &&
-          details.schedule.map((round, i) => {
+        {details?.schedule &&
+          details?.schedule.map((round, i) => {
             return (
               <div className=" space-y-2 md:flex md:space-x-2 md:space-y-0">
                 <div className="font-bold">Round {i + 1}: </div>
-                <div>{round.datetime}</div>
+                <div>{round?.datetime}</div>
               </div>
             );
           })}
       </div>
-      {details.link && (
+      {details?.link && (
         <div className="w-min">
-          <Link to={details.link}>
+          <Link to={details?.link}>
             <div className=" text-sky-400">Link</div>
           </Link>
         </div>
@@ -71,22 +61,24 @@ function RegisteredEventCard(eve) {
 }
 
 export default function Profile() {
-  const userState = useSelector(({ user }) => user);
   const [registeredEvents, setRegisteredEvents] = useState([]);
-
-  // TODO: to be taken from redux
-  const [userData, setUserData] = useState({
-    name: "Vedant Daigavane",
-    email: "daigavane70@gmail.com",
-    mobile: "+919172953517",
-    college: "PICT",
-    branch: "IT",
-  });
+  const [userData, setUserData] = useState({});
 
   const navigate = useNavigate();
 
+  async function setParticipations() {
+    await AuthVerify({
+      getParticipations: true,
+    }).then(async res => {
+      console.log(res);
+      setTimeout(() => {
+        if(!res?.loggedIn) navigate("/auth");
+      }, 1500);
+      setRegisteredEvents(res?.participations);
+    });
+  }
+
   useEffect(() => {
-    if (!userState.loggedIn) navigate("/auth");
     Requests.getUserProfile().then(({ data: { status, error, data } }) => {
       if (status) {
         setUserData(data);
@@ -94,34 +86,32 @@ export default function Profile() {
         // handle error
       }
     });
-    if (userState) {
-      setRegisteredEvents(userState?.participations);
-    }
+    setParticipations();
   }, []);
 
   return (
     <div className="md:p-4">
       <div className="min-h-screen max-w-6xl mx-auto my-8 p-4 md:p-16 space-y-8 text-gray-200 tracking-widest bg-black/40 backdrop-blur-xl">
         <div className=" font-bold text-3xl md:text-6xl text-purple-600">
-          {userData.name}
+          {userData?.name}
         </div>
 
         <div className="grid md:grid-cols-2 gap-4 text-lg">
           <div className="flex space-x-4">
             <div className="font-bold text-sm">Email: </div>
-            <div className="text-sm">{userData.email}</div>
+            <div className="text-sm">{userData?.email}</div>
           </div>
           <div className=" flex space-x-4">
             <div className="font-bold text-sm">Mobile: </div>
-            <div className="text-sm">{userData.mobile}</div>
+            <div className="text-sm">{userData?.mobile}</div>
           </div>
           <div className=" flex space-x-4">
             <div className="font-bold text-sm">College:</div>
-            <div className="text-sm">{userData.college || "N.A"}</div>
+            <div className="text-sm">{userData?.college || "N.A"}</div>
           </div>
           <div className=" flex space-x-4">
             <div className="font-bold text-sm">Branch:</div>
-            <div className="text-sm">{userData.branch || "N.A"}</div>
+            <div className="text-sm">{userData?.branch || "N.A"}</div>
           </div>
         </div>
 
@@ -130,8 +120,8 @@ export default function Profile() {
         </div>
 
         <div className=" grid md:grid-cols-2 gap-4">
-          {registeredEvents.length > 0 ? (
-            registeredEvents.map((event) => {
+          {registeredEvents?.length > 0 ? (
+            registeredEvents?.map((event) => {
               return (
                 <RegisteredEventCard details={event}></RegisteredEventCard>
               );
