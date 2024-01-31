6657fc89881796a7eb30db1204fad67315637de5
Parth Sali <parthsali04@gmail.com>
2024-02-01 00:27:31 +0530

    Update Schedule

--- src/pages/schedule/schedule.js
+++ src/pages/schedule/schedule.js
@@ -3,6 +3,7 @@ import Timer from "../../components/countdownTimer";
 import EventDay from "./scheduleComponents/eventDay.tsx";
 
 function Schedule() {
+
   const eventsSchedule = [
     [
       {
@@ -17,105 +18,111 @@ function Schedule() {
       },
       {
         name: "Campus to Corporate",
-        time: '5:00 pm - 5:30 pm',
-        platform: 'IT/ENTC Seminar Hall'
-      },
-      {
-        name: "Decrypto Hunt",
-        time: '5:30 pm - 6:30 pm',
-        platform: 'Offline'
-      },
-      {
-        name: "Codestrike",
-        time: "8:00 pm - 10:00 pm",
-        platform: "Online",
-      },
-      {
-        name: "CricWars",
-        time: "10:00 pm - 11:00 pm",
-        platform: "Online",
-      },
-    ], //*01st,
-    [
-      {
-        name: "AI Hackathon",
-        time: '3:30 pm - 4:30 pm',
-        platform: 'Auditorium'
-      },
-      {
-        name: "CricWars",
-        time: '4:00 pm - 5:00 pm',
-        platform: 'E&TC Classrooms'
+        time: '6:00 pm - 7:00 pm',
+        platform: 'Online'
       },
       {
         name: "Fandom",
-        time: '4:15 pm - 6:15 pm',
-        platform: 'IT/E&TC Classrooms'
+        time: '7:00 pm - 7:30 pm',
+        platform: 'Online'
+      },
+      {
+        name: "Codestrike",
+        time: '8:00 pm - 10:00 pm',
+        platform: 'Online'
+      },
+      {
+        name: "CricWars",
+        time: '10:00 pm - 11:00 pm',
+        platform: 'Online'
+      }
+    ],//*01st,
+    [
+      {
+        name: "Session by IMS",
+        time: '3:00 pm - 4:00 pm',
+        platform: 'Auditorium'
+      },
+      {
+        name: "Decrypto Hunt",
+        time: '4:00 pm - 5:00 pm',
+        platform: 'PICT Campus'
       },
 
       {
         name: "Campus to Corporate",
-        time: '4:45 pm - 5:45 pm',
-        platform: 'Offline'
+        time: '5:00 pm - 6:00 pm',
+        platform: 'Online '
       },
       {
         name: "Xenathon (Ideathon)",
-        time: "5:30 pm - 6:30 pm",
-        platform: "Online",
+        time: '5:30 pm - 6:30 pm',
+        platform: 'Online'
       },
       {
         name: "Xenogic",
         time: '6:00 pm - 6:30 pm',
         platform: 'Online'
-      }
+      },
+      {
+        name: "Ninja Coding",
+        time: '7:00 pm - 8:00 pm',
+        platform: 'Online'
+      },
+      {
+        name: "Code Strike",
+        time: '9:00 pm - 11:00 pm',
+        platform: 'Online'
+      },
     ],//*02nd,
     [
       {
         name: "AI Hackathon",
-        time: "To Be Decided",
-        platform: "Online",
+        time: 'To Be Decided',
+        platform: 'Online'
       },
       {
         name: "Campus to Corporate",
         time: '10:00 am - 11:00 am',
-        platform: 'E&TC Classrooms'
+        platform: 'Hybrid'
       },
       {
-        name: "Decrypto Hunt",
+        name: "CricWars",
         time: '11:00 am - 12:00 noon',
         platform: 'Offline'
       },
       {
         name: "Fandom",
-        time: '12:00 noon - 1:00 pm',
+        time: '12:00 noon - 2:00 pm',
         platform: 'E&TC Classrooms'
       },
       {
         name: "Xenogic",
-        time: "2:00 pm - 3:00 pm",
-        platform: "F-Building Classroom",
+        time: '2:00 pm - 3:00 pm',
+        platform: 'F-Building Classroom'
       },
       {
         name: "Xenalytics",
-        time: "3:30 pm - 5:30 pm",
-        platform: "F-Building Classroom",
+        time: '3:30 pm - 5:30 pm',
+        platform: 'F-Building Classroom'
       },
       {
-        name: "CricWars",
-        time: '10:00 am onwards',
-        platform: 'IT/E&TC Seminar Hall'
+        name: "Decrypto Hunt",
+        time: '4:30 pm - 5:30 pm',
+        platform: 'Offline'
       },
       {
         name: "Ninja Coding",
-        time: "9:00 pm - 11:15 pm",
-        platform: "Online",
+        time: '9:00 pm - 11:15 pm',
+        platform: 'Online'
       },
-    ], //*03rd
+
+    ],//*03rd
     [
       {
         name: "AI Hackathon",
-        time: "9:00 am - 2:00 pm",
-        platform: "Auditorium",
+        time: '9:00 am - 2:00 pm',
+        platform: 'Auditorium'
       },
       {
         name: "Cricwars",
@@ -124,8 +131,8 @@ function Schedule() {
       },
       {
         name: "Xenathon (ChannelExpo)",
-        time: "10:00 am - 12:00 noon",
-        platform: "IT/E&TC Seminar Hall",
+        time: '10:00 am - 12:00 noon',
+        platform: 'IT/E&TC Seminar Hall'
       },
       {
         name: "Fandom",
@@ -135,20 +142,20 @@ function Schedule() {
 
       {
         name: "Xenogic",
-        time: "11:00 am - 12:00 noon",
-        platform: "E&TC Classrooms",
+        time: '11:00 am - 12:00 noon',
+        platform: 'E&TC Classrooms'
       },
       {
         name: "Xenathon (Ideathon)",
-        time: "1:00 pm - 3:00 pm",
-        platform: "IT/E&TC Seminar Hall",
+        time: '1:00 pm - 3:00 pm',
+        platform: 'IT/E&TC Seminar Hall'
       },
       {
         name: "Closing Ceremony",
-        time: 'To Be Decided',
+        time: '4:00 pm - 5:00 pm',
         platform: 'Auditorium'
       },
-    ], //*04th
+    ]//*04th
   ];
 
   const renderSchedule = eventsSchedule.map((eventDay, index) => (
